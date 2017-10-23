195f4fde97efdd77b18578601b1545f5f5edb44b
Rosa Hughes <rosahughes@Rosas-MacBook-Pro.local>
2017-10-22 21:38:47 -0700

    finished project

---
+++ src/components/Secret.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="secret">
+    <h1>{{ message }}</h1>
+    <ul>
+      <li><router-link v-bind:to="{ name: 'Home' }">Home</router-link></li>
+      <li><router-link v-bind:to="{ name: 'Survey' }">Survey</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Secret',
+  data () {
+    return {
+      message: "Believe your linter when it tells you there is an error. It's never wrong."
+    }
+  }
+ }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  border: 1px solid #aa0000;
+  padding: 1rem;
+  color: #aa0000;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
